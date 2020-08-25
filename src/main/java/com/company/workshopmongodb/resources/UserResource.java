7a53be5f35333d0140f2a0e4695b3a0c2f6e7144
Diego PS <diegops.tads@gmail.com>
2020-08-25 18:37:57 -0300

    Getting user by id

--- src/main/java/com/company/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/company/workshopmongodb/resources/UserResource.java
@@ -1,13 +1,11 @@
 package com.company.workshopmongodb.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +28,12 @@ public class UserResource {
 		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO (x)).collect(Collectors.toList());
 
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 
 }
--- src/main/java/com/company/workshopmongodb/services/UserService.java
+++ src/main/java/com/company/workshopmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.company.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,11 @@ public class UserService {
 		
 		return repository.findAll();
 		
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
