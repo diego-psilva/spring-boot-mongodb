f882e6a685dec0c8c4ee7775b98614cc06573210
Diego PS <diegops.tads@gmail.com>
2020-08-26 11:57:25 -0300

    Update user

--- src/main/java/com/company/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/company/workshopmongodb/resources/UserResource.java
@@ -53,4 +53,12 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping (method = RequestMethod.PUT, value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO,@PathVariable String id){
+		User obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	
+	}
 }
